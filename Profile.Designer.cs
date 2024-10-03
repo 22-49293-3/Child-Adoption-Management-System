18b6a48dc9a5d1a206c3da18c3250b5f69e87989
22-49293-3 <22-49293-3@student.aiub.edu>
2024-10-03 23:17:54 +0600

    Add files via upload

---
+++ Profile.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Child_Adoption
+{
+    partial class Profile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelFullName = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.labelContact = new System.Windows.Forms.Label();
+            this.labelNid = new System.Windows.Forms.Label();
+            this.labelEmployeeId = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelFullName
+            // 
+            this.labelFullName.AutoSize = true;
+            this.labelFullName.Font = new System.Drawing.Font("Nirmala UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFullName.Location = new System.Drawing.Point(208, 129);
+            this.labelFullName.Name = "labelFullName";
+            this.labelFullName.Size = new System.Drawing.Size(83, 32);
+            this.labelFullName.TabIndex = 0;
+            this.labelFullName.Text = "label1";
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Font = new System.Drawing.Font("Nirmala UI", 18F, System.Drawing.FontStyle.Bold);
+            this.labelEmail.Location = new System.Drawing.Point(208, 175);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(83, 32);
+            this.labelEmail.TabIndex = 1;
+            this.labelEmail.Text = "label2";
+            // 
+            // labelContact
+            // 
+            this.labelContact.AutoSize = true;
+            this.labelContact.Font = new System.Drawing.Font("Nirmala UI", 18F, System.Drawing.FontStyle.Bold);
+            this.labelContact.Location = new System.Drawing.Point(208, 222);
+            this.labelContact.Name = "labelContact";
+            this.labelContact.Size = new System.Drawing.Size(83, 32);
+            this.labelContact.TabIndex = 2;
+            this.labelContact.Text = "label3";
+            // 
+            // labelNid
+            // 
+            this.labelNid.AutoSize = true;
+            this.labelNid.Font = new System.Drawing.Font("Nirmala UI", 18F, System.Drawing.FontStyle.Bold);
+            this.labelNid.Location = new System.Drawing.Point(208, 273);
+            this.labelNid.Name = "labelNid";
+            this.labelNid.Size = new System.Drawing.Size(83, 32);
+            this.labelNid.TabIndex = 3;
+            this.labelNid.Text = "label3";
+            // 
+            // labelEmployeeId
+            // 
+            this.labelEmployeeId.AutoSize = true;
+            this.labelEmployeeId.Font = new System.Drawing.Font("Nirmala UI", 18F, System.Drawing.FontStyle.Bold);
+            this.labelEmployeeId.Location = new System.Drawing.Point(208, 326);
+            this.labelEmployeeId.Name = "labelEmployeeId";
+            this.labelEmployeeId.Size = new System.Drawing.Size(83, 32);
+            this.labelEmployeeId.TabIndex = 4;
+            this.labelEmployeeId.Text = "label3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(207, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(334, 40);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Profile Information...";
+            // 
+            // Profile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 509);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelEmployeeId);
+            this.Controls.Add(this.labelNid);
+            this.Controls.Add(this.labelContact);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.labelFullName);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Profile";
+            this.Text = "Profile";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFullName;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.Label labelContact;
+        private System.Windows.Forms.Label labelNid;
+        private System.Windows.Forms.Label labelEmployeeId;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Profile.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Child_Adoption
+{
+    public partial class Profile : Form
+    {
+        private readonly string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\USER\\Documents\\Project.mdf;Integrated Security=True;Connect Timeout=30";
+        private string username;
+
+        public Profile(string username)
+        {
+            InitializeComponent();
+            this.username = username;
+            LoadUserInfo();
+        }
+
+     
+        private void LoadUserInfo()
+        {
+          
+            string query = "SELECT [FullName], [Email], [Contact], [Nid], [EmployeeId] FROM [User] WHERE [Username] = @username";
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@username", username);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                               
+                                labelFullName.Text ="Name: "+ reader["FullName"].ToString();
+                                labelEmail.Text ="Email: "+ reader["Email"].ToString();
+                                labelContact.Text = "Contact: " + reader["Contact"].ToString();
+                                labelNid.Text = "Nid: " + reader["Nid"].ToString();
+                                labelEmployeeId.Text = "EmployeeId: " + reader["EmployeeId"].ToString() != "" ? reader["EmployeeId"].ToString() : "N/A";
+                            }
+                            else
+                            {
+                                MessageBox.Show("User not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
